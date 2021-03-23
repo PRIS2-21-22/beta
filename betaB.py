8ed057cee9824ec3717d17867a48eb336fcc0d1b
Xespion <lemxespion@gmail.com>
2021-03-23 21:05:15 +0100

    SUMA y RESTA

--- betaB.py
+++ betaB.py
@@ -1,1 +1,36 @@
-print("This line will be printed.")
+class fuzzyNumber:
+    value = [] #Trapezoidal o Triangular
+
+    def __init__(self, m, a, b, d):
+        #Caso Triangular
+        if d == None:
+            self.value = [m,a,b]
+        else: #Caso Trapezoidal
+            self.value = [m,a,b,d]
+
+    def op_suma(self, num):
+        if len(self.value) == 3 and len(num.value) == 3:
+            return [self.value[0] + num.value[0], self.value[1] + num.value[1], self.value[2] + num.value[2]]
+        else:
+            return [self.value[0] + num.value[0], self.value[1] + num.value[1], self.value[2] + num.value[2], self.value[3] + num.value[3]]
+
+    def op_resta(self, num):
+        if(len(num.value) == 3 and len(self.value) == 3):
+            return self.op_suma(fuzzyNumber(-num.value[0], -num.value[1], -num.value[2], d=None))
+        else:
+            return self.op_suma(fuzzyNumber(-num.value[0],-num.value[1],-num.value[2],-num.value[3]))
+
+    def op_opuesto(self):
+        return
+    
+    def op_mult(self, num):
+        return
+
+    def op_div(self, num):
+        return
+
+b1 = fuzzyNumber(m=0, a=1, b=2, d=3)
+b2 = fuzzyNumber(m=1, a=2, b=0, d=3)
+bSuma = b1.op_suma(b2)
+bResta = b1.op_resta(b2)
+print(bResta)
