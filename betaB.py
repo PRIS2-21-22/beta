21664a6059139a789e049ee9c97ecb358b79937c
Xespion <lemxespion@gmail.com>
2021-03-24 19:48:30 +0100

    Ahora si

--- betaB.py
+++ betaB.py
@@ -35,12 +35,12 @@ class fuzzyNumber:
 
     def op_div(self, num):
         if(len(num.value) == 3 and len(self.value) == 3):
-            return self.op_suma(fuzzyNumber(-num.value[0], -num.value[1], -num.value[2], d=None))
+            return [self.value[0]/num.value[0], (num.value[1]*self.value[0] + self.value[1]*num.value[0])/num.value[0]**2, (num.value[2]*self.value[0] + self.value[2]*num.value[0])/num.value[0]**2]
         else:
-            return self.op_suma(fuzzyNumber(-num.value[0],-num.value[1],-num.value[2],-num.value[3]))
+            return [self.value[0]/num.value[0], (num.value[1]*self.value[0] + self.value[1]*num.value[0])/num.value[0]**2, (num.value[2]*self.value[0] + self.value[2]*num.value[0])/num.value[0]**2, (num.value[3]*self.value[0] + self.value[3]*num.value[0])/num.value[0]**2]
 
 b1 = fuzzyNumber(m=0, a=1, b=2, d=3)
 b2 = fuzzyNumber(m=1, a=2, b=0, d=3)
 bSuma = b1.op_suma(b2)
-bResta = b1.op_resta(b2)
+bResta = b1.op_div(b2)
 print(bResta)
