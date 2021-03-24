d4c6af33542ef73540631f29cca68d6e33a0af2e
Xespion <lemxespion@gmail.com>
2021-03-24 19:24:02 +0100

    2º commit, apparently over no se si cambiare algo mas pero esta listo

--- betaB.py
+++ betaB.py
@@ -8,6 +8,7 @@ class fuzzyNumber:
         else: #Caso Trapezoidal
             self.value = [m,a,b,d]
 
+    #De momento solo estan los casos en los que ambos son triangulares o ambos son trapezoidales
     def op_suma(self, num):
         if len(self.value) == 3 and len(num.value) == 3:
             return [self.value[0] + num.value[0], self.value[1] + num.value[1], self.value[2] + num.value[2]]
@@ -21,13 +22,22 @@ class fuzzyNumber:
             return self.op_suma(fuzzyNumber(-num.value[0],-num.value[1],-num.value[2],-num.value[3]))
 
     def op_opuesto(self):
-        return
+        if len(self.value) == 3:
+            return [-self.value[0], self.value[2], self.value[1]]
+        else:
+            return [-self.value[0], self.value[2], self.value[1], -self.value[3]] 
     
     def op_mult(self, num):
-        return
+        if len(self.value) == 3 and len(num.value) == 3:
+            return [self.value[0]*num.value[0],self.value[1]*num.value[1],self.value[2]*num.value[2]]
+        else:
+            return [self.value[0]*num.value[0],self.value[1]*num.value[1],self.value[2]*num.value[2],self.value[3]*num.value[3]]
 
     def op_div(self, num):
-        return
+        if(len(num.value) == 3 and len(self.value) == 3):
+            return self.op_suma(fuzzyNumber(-num.value[0], -num.value[1], -num.value[2], d=None))
+        else:
+            return self.op_suma(fuzzyNumber(-num.value[0],-num.value[1],-num.value[2],-num.value[3]))
 
 b1 = fuzzyNumber(m=0, a=1, b=2, d=3)
 b2 = fuzzyNumber(m=1, a=2, b=0, d=3)
